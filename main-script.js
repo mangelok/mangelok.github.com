366e255924924bf018e303f6cc580fb019736049
Michael Knight <mangelok@gmail.com>
2014-04-26 14:36:17 -0700

    Fixing navbar

--- main-script.js
+++ main-script.js
@@ -1,4 +1,4 @@
-var navBar = "<table id='navbar'>	<tr>		<td>			<h2>links</h2>			<a href='index.html'>home</a><br>			<a href='resources/resume.pdf'>résumé</a><br>			<a href='mailto:m3knight@uwaterloo.ca'>email</a><br>			<a href='http://www.linkedin.com/in/mangelok'>linkedin</a><br>		</td>		<td id='navbar-separator'></td>		<td>			<h2>projects</h2>			<a href='mydirections.htm'>myDirections</a><br>			<a href='invoicer.htm'>Invoicer</a><br>			<a href='smd.htm'>Super Mario Defense</a><br>			<a href='ml.htm'>Mario LOGO</a><br>		</td>	</tr></table>";
+var navBar = "<table id='navbar'>	<tr>		<td>			<h2>links</h2>			<a href='index.html'>home</a><br>			<a href='resources/resume.pdf'>resume</a><br>			<a href='mailto:m3knight@uwaterloo.ca'>email</a><br>			<a href='http://www.linkedin.com/in/mangelok'>linkedin</a><br>		</td>		<td id='navbar-separator'></td>		<td>			<h2>projects</h2>			<a href='mydirections.htm'>myDirections</a><br>			<a href='invoicer.htm'>Invoicer</a><br>			<a href='smd.htm'>Super Mario Defense</a><br>			<a href='ml.htm'>Mario LOGO</a><br>		</td>	</tr></table>";
 
 function loadNavBar()
 {
@@ -23,4 +23,5 @@ function loadNavBar()
 	page = page.substring(0,page.length);
 	var navhtml = document.getElementById("menu");
 	navhtml.innerHTML = navhtml.innerHTML.replace("href=\""+page,"id=\"thisLink\" href=\"javascript: void(0)");
+	console.log(navhtml);
 }
