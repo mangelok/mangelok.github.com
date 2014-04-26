1e3ee5cbefd939236d5eb5c26fbbd1fecd047daf
Michael Knight <mangelok@gmail.com>
2014-04-26 13:30:18 -0700

    Added some javascript

---
+++ main-script.js
@@ -0,0 +1,10 @@
+function loadPage(href)
+{
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", href, false);
+	xmlhttp.send();
+	return xmlhttp.responseText;
+}
+function loadNavBar(){
+	return loadPage("navbar.html");
+}
